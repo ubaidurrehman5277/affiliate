10f8cff8b4712f68b2d6cb4514f152ab41da68e3
ubaidurrehman5277 <obaidurrehman841@gmail.com>
2023-11-13 10:57:55 +0500

    footer updated with chat

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -65,6 +65,21 @@
 	<script>
 		new WOW().init();
 	</script>
+
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6550a00acec6a912820f0ba6/1hf1f2at2';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
